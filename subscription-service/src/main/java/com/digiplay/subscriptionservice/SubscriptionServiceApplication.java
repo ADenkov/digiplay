f0467d7262c4fc77be7b0d527faac456eab3b76e
antonio.d <antonio.denkov@devsecops-academy.com>
2022-06-14 10:53:44 +0200

    fix subscription service

--- subscription-service/src/main/java/com/digiplay/subscriptionservice/SubscriptionServiceApplication.java
+++ subscription-service/src/main/java/com/digiplay/subscriptionservice/SubscriptionServiceApplication.java
@@ -2,10 +2,10 @@ package com.digiplay.subscriptionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+//@EnableEurekaClient
 public class SubscriptionServiceApplication {
 
 	public static void main(String[] args) {
